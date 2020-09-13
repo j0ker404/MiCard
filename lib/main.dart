16a34708303bf053b8883d3b61741f331df1cabd
joker <uanhermanus@gmail.com>
2020-09-13 02:28:05 +0200

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,158 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                // backgroundColor: Colors.red,
+                backgroundImage: AssetImage("images/person.jpeg"),
+              ),
+              Text(
+                "Bob Stone",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  letterSpacing: 3.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // 'hr' basicly
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+123456789',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'mymail@mail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class Challenge1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                // height: 100,
+                child: Center(
+                  child: Text("C1"),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100,
+                    height: 100,
+                    child: Center(
+                      child: Text("C2"),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    child: Center(
+                      child: Text("C3"),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                // height: 100,
+                child: Center(
+                  child: Text("C4"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
